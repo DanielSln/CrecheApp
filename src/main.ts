42e1a60129905cb7488c86886f616a17a626db53
DanielSln <12danielsolano@gmail.com>
2025-10-29 22:38:36 -0300

    feat: adicionar suprimir de warnings de acessibilidade no console durante o desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
-// Desabilita warnings de acessibilidade do Ionic
-if (typeof window !== 'undefined') {
-  (window as any).Ionic = { config: { _testing: true } };
-}
-
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
