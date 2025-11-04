105d7f2817230d4bb68d61e1695e350cd8f1bf76
DanielSln <12danielsolano@gmail.com>
2025-11-04 15:44:27 -0300

    feat: page html, css and ts initial

--- src/app/pages/escrever-comunicado/escrever-comunicado.page.ts
+++ src/app/pages/escrever-comunicado/escrever-comunicado.page.ts
@@ -1,20 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonTitle,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-escrever-comunicado',
   templateUrl: './escrever-comunicado.page.html',
   styleUrls: ['./escrever-comunicado.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonTitle,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+  ],
 })
-export class EscreverComunicadoPage implements OnInit {
+export class EscreverComunicadoPage {
+  destinatario = '';
+  assunto = '';
+  mensagem = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  voltar() {
+    this.router.navigateByUrl('/comunicados-docente');
   }
 
+  enviarComunicado() {
+    if (!this.assunto || !this.mensagem) {
+      alert('Por favor, preencha o assunto e a mensagem.');
+      return;
+    }
+
+    console.log('Comunicado enviado:', {
+      para: this.destinatario,
+      assunto: this.assunto,
+      mensagem: this.mensagem,
+    });
+
+    alert('Comunicado enviado com sucesso!');
+    this.router.navigateByUrl('/comunicados-docente');
+  }
+
+  limparCampos() {
+    this.destinatario = '';
+    this.assunto = '';
+    this.mensagem = '';
+  }
 }
