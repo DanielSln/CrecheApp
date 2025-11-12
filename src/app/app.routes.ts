d9975544561b867beedef8393a773019b1724e93
Anthony3043 <am1664827@gmail.com>
2025-11-12 16:11:40 -0300

    rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/comunicados-docente',
+    redirectTo: '/login-aluno',
     pathMatch: 'full',
   },
 
